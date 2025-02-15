f8bdd436102e02a06052eb9282e8ecaca9db4667
xet1309 <xavier.tromp@student.hu.nl>
2025-02-15 22:16:37 +0100

    cleaned up file structure a bit

---
+++ inno-frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "./services/auth/auth_firebase_config.js";
+
+function App() {
+
+    const handleClick = async () => {
+        const provider = new GoogleAuthProvider();
+        const result = await signInWithPopup(auth, provider);
+        console.log(result.user);
+    }
+
+  return (
+    <>
+      <button onClick={handleClick} >Click to login with Google</button>
+    </>
+  )
+}
+
+export default App
