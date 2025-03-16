b5426638bb7ff9a07878f2617079fcce8914f1f7
xet1309 <xavier.tromp@student.hu.nl>
2025-03-16 16:41:56 +0100

    added code to config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// https://stackoverflow.com/a/77643886
 export default defineConfig({
   plugins: [react()],
   base: "/learningstories-xet1309/",
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  build: {
+    target: 'esnext'
+  },
 })
